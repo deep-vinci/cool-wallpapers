79e221f150bacb039a5cb3071f916ca82762a96a
Deepak <deepakworkpc@gmail.com>
2024-07-16 17:43:26 +0530

    pushing 1.3gb of wallpapers may the god take care of the bytes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cool-wallpapers
+
+
+![](/452-desktop.png)
+![](/626-desktop.png)
+![](/603-desktop.png)
+
+.. hundreds like these
---
+++ facets-li-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+baseurl="https://d2j7mtsoydewxq.cloudfront.net/facets-ii"
+
+# Loop from 366 to 730 as by manual sources inspection i only found that many urls
+for i in $(seq 591 730); do
+
+  url="${baseurl}/${i}-desktop.png"
+  filename="${i}-desktop.png"
+
+
+  echo "Downloading ${filename} from ${url}"
+
+  curl -s -o "$filename" "$url"
+done
+
+echo "Download complete."
