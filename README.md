89e8e3b588dbc6622a1e9ce6f4b079f5cddd78f6
Deepak <deepakworkpc@gmail.com>
2024-07-16 17:58:00 +0530

    fix: correct loop iterator , docs: update the readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cool-wallpapers
 
+- Script to download all the wallpapers locally in [/facets-li-1.sh](/facets-li-1.sh)
 
 ![](/452-desktop.png)
 ![](/626-desktop.png)
--- facets-li-1.sh
+++ facets-li-1.sh
@@ -3,7 +3,7 @@
 baseurl="https://d2j7mtsoydewxq.cloudfront.net/facets-ii"
 
 # Loop from 366 to 730 as by manual sources inspection i only found that many urls
-for i in $(seq 591 730); do
+for i in $(seq 366 730); do
 
   url="${baseurl}/${i}-desktop.png"
   filename="${i}-desktop.png"
